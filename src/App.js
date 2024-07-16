159dafaa0df6152259b8184fcd878a26e955e475
saurabh30 <saurabhc.0102@gmail.com>
2024-07-16 14:07:59 +0530

    Note_app_frontend

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { HashRouter, Route, Routes, Navigate  } from 'react-router-dom';
+import NotesPage from './NotesPage';
+import Home from './Home';
+import Login from './Componnets/Login';
+import Signup from './Componnets/Signup';
+
+
+function App() {
+
+  const [authenticated, setAuthenticated] = useState(false);
+
+  return (
+    <HashRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login setAuthenticated={setAuthenticated}/>} />
+          <Route path="/notes" element={<NotesPage/>} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </HashRouter>
+
+  );
+}
+
+export default App;
---
+++ src/Componnets/Login.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const Login = ({setAuthenticated }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('https://note-app-backend-31cy.onrender.com/api/users/login', { username, password });
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('user', JSON.stringify(response.data.user));
+            navigate('/notes');
+            // setAuthenticated(true);
+        } catch (error) {
+            console.error('Failed to login:', error);
+        }
+      };
+
+    return (
+        <div className="Auth-form-container">
+            <form className="Auth-form" onSubmit={handleLogin}>
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Log In</h3>
+                    <div className="text-center">
+                        Not registered yet?{" "}
+                        <a href="/signup">
+                            Sign Up
+                        </a>
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>UserName</label>
+                        <input
+                            type="text"
+                            className="form-control mt-1"
+                            placeholder="Enter email"
+                            value={username} onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Enter password"
+                            value={password} onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                    <p className="text-center mt-2">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+
+
+    );
+};
+
+export default Login;
